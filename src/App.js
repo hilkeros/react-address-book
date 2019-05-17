fe1e57e8ea8718365b44132975f11b3b6cc78207
Hilke Ros <hilke@mooris.ch>
2019-05-17 20:17:58 +0200

    basic infinite scroll and bootstrap. far too many api calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import {Container, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 const API = 'https://randomuser.me/api/';
@@ -14,10 +16,12 @@ class App extends Component {
 
     this.state = {
       users: [],
+      hasMoreItems: true
     };
   }
 
-  componentDidMount() {
+  loadItems() {
+    console.log('loading');
     fetch(API + DEFAULT_QUERY) 
       .then(response => response.json())
       .then(data => this.setState({ users: data.results }));
@@ -25,16 +29,37 @@ class App extends Component {
 
   render() {
     const { users } = this.state;
+    const loader = <Row className="loader">Loading ...</Row>;
+
+    var items = [];
+    users.map((user, i) => {
+        items.push(
+            <Col xs={6} md={3} className="thumb" key={i}>
+              <a href="#" target="_blank">
+                  <img src={user.picture.large} width="128" height="128" />
+                  <div className="name">{user.name.first} {user.name.last}</div>
+              </a>
+              <div className="login">{user.login.username}</div>
+              <div className="email">{user.email}</div> 
+            </Col>
+        );
+    });
 
     return (
-      <ul>
-        {users.map(user =>
-          <li>
-            {user.email}
-          </li>
-        )}
-      </ul>
+        <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadItems.bind(this)}
+            hasMore={this.state.hasMoreItems}
+            loader={loader}>
+
+            <Container>
+              <Row>
+                {items}
+              </Row>
+            </Container>
+        </InfiniteScroll>
     );
+  
   }
 
 }
