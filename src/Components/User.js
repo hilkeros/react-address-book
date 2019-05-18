5781c24a34387588d933c36ef1925e236b254265
Hilke Ros <hilke@mooris.ch>
2019-05-18 15:38:50 +0200

    user detail modal

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 
+import UserDetail from './UserDetail'
+
 class User extends Component {
+	constructor(...args) {
+		super(...args);
+		this.state = { modalShow: false}
+	}
 	render() {
 		let user = this.props.user;
+		let modalClose = () => this.setState({ modalShow: false });
+
 		return (
 			<Col xs={6} md={3} className="thumb" key={user.login.uuid}>
-			  <a href="#" target="_blank">
+			  <a href='#' onClick={() => this.setState({ modalShow: true })}>
 			      <img src={user.picture.large} width="128" height="128" />
 			      <div className="name">{user.name.first} {user.name.last}</div>
 			  </a>
 			  <div className="login">{user.login.username}</div>
 			  <div className="email">{user.email}</div> 
+
+			  <UserDetail
+          user={user}
+          show={this.state.modalShow}
+          onHide={modalClose}
+        />
 			</Col>
 		)
 	}
---
+++ src/Components/UserDetail.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {Modal, Button, Container, Row, Col} from 'react-bootstrap';
+
+class UserDetail extends Component {
+	render() {
+	    let user = this.props.user;
+	    return (
+	      <Modal
+	        {...this.props}
+	        size="md"
+	        aria-labelledby="contained-modal-title-vcenter"
+	        centered
+	      >
+	        <Modal.Header closeButton>
+	          <Modal.Title id="contained-modal-title-vcenter">
+	            {user.name.first} {user.name.last}
+	          </Modal.Title>
+	        </Modal.Header>
+	        <Modal.Body>
+	          <Container>
+	          	<Row>
+	          		<Col>
+	          			<img src={user.picture.large} width="128" height="128" />
+          			</Col>
+          			<Col>
+				          <p>
+				            {user.location.street}<br/>
+				            {user.location.postcode} {user.location.city}<br/>
+				            {user.phone}<br/>
+				            {user.cell}<br/>
+				          </p>
+			          </Col>
+		          </Row>
+	          </Container>
+	        </Modal.Body>
+	        <Modal.Footer>
+	          <Button onClick={this.props.onHide}>Close</Button>
+	        </Modal.Footer>
+	      </Modal>
+	    );
+	  }
+}
+
+export default UserDetail
