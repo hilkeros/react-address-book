eb22189a16d1690a536a15315de7074906ffc4bf
Hilke Ros <hilke@mooris.ch>
2019-05-19 20:52:11 +0200

    prefetch next batch from API

--- src/App.js
+++ src/App.js
@@ -14,32 +14,50 @@ class App extends Component {
 
     this.state = {
       users: [],
+      shownUsers: [],
       hasMoreItems: true,
-      page: 1
+      page: 1,
+      cursor: 0
     };
   }
 
   loadItems() {
-    let url = API + DEFAULT_QUERY + '&page=' + this.state.page
+    let cursor = this.state.cursor;
+    let hasMoreItems = this.state.hasMoreItems;
+    let shownUsers = this.state.shownUsers;
+     console.log('shown ' + shownUsers.length);
+     console.log('loaded ' + this.state.users.length);
+    if (this.state.users.length > 49){
+      shownUsers = this.state.shownUsers.concat(this.state.users.slice(cursor, cursor + 50));
+      cursor = cursor + 50;
+    } 
+    if ( this.state.users.length > 999) {
+      console.log('stop');
+      hasMoreItems = false;
+    }
+    let url = API + DEFAULT_QUERY + '&page=' + this.state.page;
+    console.log('fetching ' + url);
     fetch(url) 
       .then(response => response.json())
       .then(data => 
         {
           this.setState({
+            shownUsers: shownUsers,
+            cursor: cursor,
+            hasMoreItems: hasMoreItems,
             users: this.state.users.concat(data.results),
-            page: this.state.page + 1 
+            page: this.state.page + 1
         })
     });
   }
 
   render() {
-    const { users } = this.state;
     const loader = <div className="loader">Loading ...</div>;
 
     let items = [];
-    users.map((user, i) => {
+    this.state.shownUsers.map((user, i) => {
         items.push(
-            <User user={user} />
+            <User user={user} key={user.login.uuid} />
         );
     });
 
